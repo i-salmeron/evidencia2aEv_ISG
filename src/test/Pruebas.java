62e5abed4c20d655199cc11c53c8a89b071316a7
Nacho <ignacio.salmeron@campusfp.es>
2023-03-21 11:37:02 +0100

    prueba caja negra ISG

--- src/test/Pruebas.java
+++ src/test/Pruebas.java
@@ -11,8 +11,28 @@ class Pruebas {
 	CuentaBancaria cb = new CuentaBancaria();
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test1() throws Exception {
+		CuentaBancaria cb=new CuentaBancaria();
+		cb.agregarSaldo(5000);
+		cb.solicitarPrestamo(10000);
+		assertEquals(15000, cb.obtenerSaldo());
+	}
+	
+	@Test
+	void test2() throws Exception {
+		CuentaBancaria cb=new CuentaBancaria();
+		cb.agregarSaldo(2000);
+		cb.solicitarPrestamo(12000);
+		assertEquals(6000, cb.obtenerPrestamo());
+		
+	}
+	
+	@Test
+	void test3() throws Exception {
+		CuentaBancaria cb=new CuentaBancaria();
+		cb.agregarSaldo(1000);
+		cb.solicitarPrestamo(2000);
+		assertThrows(Exception.class, ()->cb.solicitarPrestamo(4000));
 	}
 
 }
